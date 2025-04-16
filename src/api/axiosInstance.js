1913903deacaa44b6227102346fbd1428e3ebedd
Babloo Kumar <97720510+Babloo321@users.noreply.github.com>
2025-04-17 00:15:55 +0530

    Update axiosInstance.js

--- src/api/axiosInstance.js
+++ src/api/axiosInstance.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 // const BASE_URL = "http://localhost:4040/api/v2";
-const BASE_URL = "https://krishi-mitra-backend.onrender.com/api/v2";
+const BASE_URL = "https://krishi-mitra-backend-1.onrender.com/api/v2";
 export const AxiosPublic = axios.create({
   baseURL: BASE_URL,
 })
